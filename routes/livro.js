cc4f95b765dee02a8cb524c1d8fd01593e409cd1
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-01 16:58:11 -0300

    Add POST, PATCH, and DELETE routes for livros
    Implemented POST, PATCH, and DELETE endpoints in routes/livro.js to handle creating, updating, and deleting books. Each route currently returns a simple confirmation message.

--- routes/livro.js
+++ routes/livro.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) =>{
     res.send('Aqui serão exibidos os livros')
 })
 
+router.post('/', (req,res) => {
+    res.send('Os livros foram postados')
+})
+
+router.patch('/', (req,res) => {
+    res.send('Os livros foram atualizados')
+})
+
+router.delete('/', (req,res) => {
+    res.send('Os livros foram deletados')
+})
+
 module.exports = router 
